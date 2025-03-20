885095e30afbc7d741cbd5c94bf41f36529bf6ce
g-i-kala <5star@wp.pl>
2025-03-20 12:28:42 +0100

    feat:  added database PDO connection

--- config/database.php
+++ config/database.php
@@ -7,8 +7,40 @@ require __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->load();
 
-$dbHost = getenv('DB_HOST');
-$dbName = getenv('DB_NAME');
-$dbUser = getenv('DB_USER');
-$dbPassword = getenv('DB_PASSWORD');
+class Database {
+    
+    private $dbHost;
+    private $dbName;
+    private $dbUser;
+    private $dbPassword;
+    private $charset = 'utf8mb4';
+    public $conn;
 
+    public function __construct() {
+        $this->dbHost = $_ENV['DB_HOST'];
+        $this->dbName = $_ENV['DB_NAME']; 
+        $this->dbUser = $_ENV['DB_USER'];
+        $this->dbPassword = $_ENV['DB_PASS'];
+    }
+
+    public function connect() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO('mysql:dbname=' . $this->dbName . ';host=' . $this->dbHost, $this->dbUser, $this->dbPassword);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
+
+        } catch (PDOException $e) {
+            echo "Connection failed. Error: " . $e->getMessage();
+            error_log($e->getMessage());
+            http_response_code(500);
+            exit;
+        }
+        return $this->conn;
+    }
+
+}
+
+
+// $database = new Database();
+// $conn = $database->connect();
+// echo var_dump($conn);
